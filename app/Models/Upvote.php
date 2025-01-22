29bfec8eaf6e460baec23da277a6b56f3945080e
Chidozie <chidough14@gmail.com>
2025-01-22 12:27:13 +0100

    Define models/migrations

---
+++ app/Models/Upvote.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Upvote extends Model
+{
+    public function feature(): BelongsTo {
+        return $this->belongsTo(Feature::class);
+    }
+}
